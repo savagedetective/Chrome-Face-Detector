536c20e02a9ed9dc0dfc8a942a8c874fe10cb56f
Ritchie <detectives.salvajes@protonmail.com>
2020-05-21 19:53:43 -0400

    base file set up. need chrome from here

--- pixelated-face.js
+++ pixelated-face.js
@@ -1,1 +1,13 @@
 console.log("Goliath online.");
+
+const video = document.querySelector(".webcam");
+
+const canvas = document.querySelector(".video");
+const ctx = canvas.getContext("2d")
+
+const faceCanvas = document.querySelector(".face");
+const faceCtx = canvas.getContext("2d")
+
+const faceDetector = new window.FaceDetector();
+
+console.log(video, canvas, faceCanvas, faceDetector)
